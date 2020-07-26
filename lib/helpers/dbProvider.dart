f6797786012b89a5c0d01d5cc6369377cd6c4de1
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-07-26 21:09:52 +0800

    ✨ DB Setup

---
+++ lib/helpers/dbProvider.dart
@@ -0,0 +1,127 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:io';
+
+// custom package
+import 'package:isa/models/Item.dart';
+
+class DBProvider {
+  // =========================================================================
+  // SINGLETON
+  // =========================================================================
+  static final DBProvider _dbProvider = DBProvider._internal();
+  static Database _db;
+
+  DBProvider._internal();
+
+  factory DBProvider() {
+    return _dbProvider;
+  }
+
+  // =========================================================================
+  // GETTERS
+  // =========================================================================
+
+  Future<Database> get db async {
+    if (_db == null) {
+      _db = await initDB();
+    }
+
+    return _db;
+  }
+
+  // =========================================================================
+  // ITEM TABLE
+  // =========================================================================
+  String tableName = 'item';
+
+  // =========================================================================
+  // ITEM TABLE COLUMNS
+  // =========================================================================
+
+  // item details
+  String id = 'id';
+  String name = 'name';
+  String price = 'price';
+  String imageUrl = 'imageUrl';
+
+  // customer information
+  String customerName = 'customerName';
+  String customerContactNumber = 'customerContactNumber';
+
+  // miscellaneous
+  String notes = 'notes';
+  String createdAt = 'createdAt';
+
+  // =========================================================================
+  // INIT DB
+  // =========================================================================
+
+  Future<Database> initDB() async {
+    Directory dir = await getApplicationDocumentsDirectory();
+    String path = dir.path + 'isa.db';
+
+    var dbISA = await openDatabase(path, version: 1, onCreate: _onCreate);
+
+    return dbISA;
+  }
+
+  void _onCreate(Database db, int newVersion) async {
+    await db.execute('''
+        create table $tableName(
+          $id integer primary key, 
+          $name text, 
+          $price decimal, 
+          $imageUrl text, 
+          $customerName text, 
+          $customerContactNumber text, 
+          $notes text, 
+          $createdAt text
+        )''');
+  }
+
+  // =========================================================================
+  // !!! QUERY METHODS HERE !!!
+  // =========================================================================
+
+  // add item
+  Future<int> insertItem(Item item) async {
+    Database db = await this.db;
+
+    var result = await db.insert(tableName, item.toMap());
+
+    return result;
+  }
+
+  // get items
+  Future<List> getItems() async {
+    Database db = await this.db;
+
+    List<Map<String, dynamic>> result = await db.query(
+      tableName,
+      orderBy: 'createdAt asc',
+    );
+
+    return result;
+  }
+
+  // update item
+  Future<int> updateItem(Item item) async {
+    Database db = await this.db;
+
+    var result = await db.update(tableName, item.toMap(),
+        where: '$id = ?', whereArgs: [item.id]);
+
+    return result;
+  }
+
+  // delete item
+  Future<int> deleteItem(int id) async {
+    Database db = await this.db;
+
+    var result = await db.delete(tableName, where: '$id = ?', whereArgs: [id]);
+
+    return result;
+  }
+}
---
+++ lib/models/Item.dart
@@ -0,0 +1,113 @@
+class Item {
+  // =========================================================================
+  // FIELDS
+  // =========================================================================
+
+  // item details
+  int _id;
+  String _name;
+  int _price;
+  String _imageUrl;
+
+  // customer information
+  String _customerName;
+  String _customerContactNumber;
+
+  // miscellaneous
+  String _notes;
+  String _createdAt;
+
+  // =========================================================================
+  // CONSTRUCTORS
+  // =========================================================================
+
+  // without id
+  Item(this._name, this._price, this._createdAt,
+      [this._imageUrl,
+      this._customerName,
+      this._customerContactNumber,
+      this._notes]);
+
+  // with id
+  Item.withId(this._name, this._price, this._createdAt,
+      [this._imageUrl,
+      this._customerName,
+      this._customerContactNumber,
+      this._notes]);
+
+  // =========================================================================
+  // GETTERS
+  // =========================================================================
+
+  int get id => _id;
+  String get name => _name;
+  int get price => _price;
+  String get imageUrl => _imageUrl;
+  String get customerName => _customerName;
+  String get customerContactNumber => _customerContactNumber;
+  String get notes => _notes;
+  String get createdAt => _createdAt;
+
+  // =========================================================================
+  // SETTERS
+  // =========================================================================
+
+  set name(String newName) {
+    if (newName.length <= 255) {
+      _name = newName;
+    }
+  }
+
+  set price(int newPrice) {
+    if (newPrice > 0) {
+      _price = newPrice;
+    }
+  }
+
+  set customerName(String newCustomerName) {
+    if (newCustomerName.length <= 255) {
+      _customerName = newCustomerName;
+    }
+  }
+
+  set customerContactNumber(String newCustomerContactNumber) {
+    // 11 is equal to the total length of contact number in PH
+    // If the app will be internationalize this has to change
+    if (newCustomerContactNumber.length == 11) {
+      _customerContactNumber = newCustomerContactNumber;
+    }
+  }
+
+  // =========================================================================
+  // HELPERS
+  // =========================================================================
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = {
+      'name': _name,
+      'price': _price,
+      'imageUrl': _imageUrl,
+      'customerName': _customerName,
+      'customerContactNumber': _customerContactNumber,
+      'notes': _notes,
+      'createdAt': _createdAt,
+    };
+
+    if (_id != null) {
+      map['id'] = _id;
+    }
+
+    return map;
+  }
+
+  Item.fromObject(dynamic o) {
+    this._id = o['id'];
+    this._name = o['name'];
+    this._price = o['price'];
+    this._imageUrl = o['imageUrl'];
+    this._customerName = o['customerName'];
+    this._customerContactNumber = o['customerContactNumber'];
+    this._notes = o['notes'];
+    this._createdAt = o['createdAt'];
+  }
+}
