f2677cbc452a78e7a3687f6eb699f408c9b35762
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-07-30 09:49:17 +0800

    🐛 Supported image base64 format

--- lib/widgets/IsaListViewItem.dart
+++ lib/widgets/IsaListViewItem.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:typed_data';
+import 'dart:convert';
 
 // =========================================================================
 // Isa ListView Item
@@ -13,6 +15,12 @@ class IsaListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Uint8List _bytesImage;
+
+    if (this.imageUrl != null) {
+      _bytesImage = Base64Decoder().convert(this.imageUrl);
+    }
+
     return Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
@@ -21,11 +29,13 @@ class IsaListViewItem extends StatelessWidget {
             decoration: BoxDecoration(
               border: Border.all(color: Theme.of(context).primaryColor),
             ),
-            child: Image(
-              width: 60,
-              height: 60,
-              image: NetworkImage(this.imageUrl),
-            ),
+            child: (this.imageUrl != null)
+                ? Image.memory(
+                    _bytesImage,
+                    width: 60,
+                    height: 60,
+                  )
+                : Icon(Icons.camera_alt),
           ),
           Container(
             width: MediaQuery.of(context).size.width * 0.38647343,
@@ -37,7 +47,7 @@ class IsaListViewItem extends StatelessWidget {
           ),
           Container(
             width: MediaQuery.of(context).size.width * 0.193236715,
-            child: Text('₱${this.price.toString()}',
+            child: Text('${this.price.toString()}',
                 textAlign: TextAlign.end,
                 style: Theme.of(context)
                     .textTheme
